0e4db472b9882e95ce21d30254ed9386d9dfdc55
tazomatalax <68450434+tazomatalax@users.noreply.github.com>
2024-11-01 18:58:38 +1300

    Update RandomNumberCard.js

--- src/components/RandomNumberCard.js
+++ src/components/RandomNumberCard.js
@@ -1,42 +1,58 @@
 import React, { useState, useEffect } from 'react';
+import CopyButton from './CopyButton';
+import { getFourDigitCode, getParsedPizzaCode } from '../utils/pizzaCodeGenerator';
 
-function RandomNumberCard(props) {
-    const [randomNumber, setrandomNumber] = useState(0);
+function RandomNumberCard({ title, startingNumber }) {
+    const [randomNumber, setRandomNumber] = useState(0);
+    const [copied, setCopied] = useState(false);
     
     useEffect(() => {
         getPizzaCode();
     }, []);
-  
-    const getFourDigitCode = () => {
-      let code = 1;
-      while (code % 3 !== 0) {
-        code = Math.floor(Math.random() * (9999-1000)) + 1000;
-      }
-      return code+1;
-    }
-  
-    const getParsedPizzaCode = (pizza_code) => {
-      return parseInt(props.startingNumber+pizza_code.toString());
-    }
     
     function getPizzaCode() {
       let randomNumber = getFourDigitCode();
-      let parsedPizzaCode = getParsedPizzaCode(randomNumber);
+      let parsedPizzaCode = getParsedPizzaCode(randomNumber, startingNumber);
       while(parsedPizzaCode % 3 !== 0) {
         randomNumber = getFourDigitCode();
-        parsedPizzaCode = getParsedPizzaCode(randomNumber);
+        parsedPizzaCode = getParsedPizzaCode(randomNumber, startingNumber);
       }
   
-      setrandomNumber(parsedPizzaCode);
+      setRandomNumber(parsedPizzaCode);
+      setCopied(false);
     }
+
+    const copyToClipboard = async () => {
+      try {
+        await navigator.clipboard.writeText(randomNumber.toString());
+        setCopied(true);
+        setTimeout(() => setCopied(false), 2000);
+      } catch (err) {
+        console.error('Failed to copy:', err);
+      }
+    };
 
     return (
         <div className='randomNumberCard'>
-            <h5>{props.title}</h5>
-            <h1 className = "randomNumber">{randomNumber}</h1>
-            <button className = "randomNumberButton" onClick = {getPizzaCode}><b>More Pizza!</b></button>
+            <h5>{title}</h5>
+            <div className="codeContainer">
+                <div 
+                    className="randomNumber" 
+                    onClick={copyToClipboard} 
+                    title="Click to copy"
+                >
+                    {randomNumber}
+                </div>
+                <CopyButton onClick={copyToClipboard} />
+            </div>
+            <div className={`copied ${copied ? 'show' : ''}`}>
+                Copied! ✓
+            </div>
+            <button className="randomNumberButton" onClick={getPizzaCode}>
+                <b>More Pizza!</b>
+            </button>
         </div>
-    )
+    );
 }
 
-export default RandomNumberCard
+export default RandomNumberCard;
