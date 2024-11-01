30b99f03284bb83ff0ab2c1bc06ab6cd111cf388
tazomatalax <68450434+tazomatalax@users.noreply.github.com>
2024-11-01 19:00:07 +1300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import RandomNumberCard from './components/RandomNumberCard';
 function App() {
   return (
     <>
-      <div className = "container">
-        <div className = 'cardContainer'>
+      <div className="container">
+        <div className='cardContainer'>
           <RandomNumberCard title="Old style code" startingNumber="2"/>
           <RandomNumberCard title="New code #1" startingNumber="5"/>
           <RandomNumberCard title="New code #2" startingNumber="8"/>
         </div>
-        <h5 className = "infoLink"> <a href = "https://github.com/omkarkaul/free-pizza">What even is this?</a></h5>
+        <h5 className="infoLink">
+          <a href="https://github.com/omkarkaul/free-pizza">What even is this?</a>
+        </h5>
       </div>
 
-      <div className = "footer">
-        <p className = "sig">Made by Omkar :)</p>
+      <div className="footer">
+        <p className="sig">Made by Omkar :)</p>
       </div>
     </>
   );
